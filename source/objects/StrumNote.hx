7c5d7f7664cfdbff0eba8eda32d2749526c063ae
Nikogamer184 <nicsteph547@gmail.com>
2023-08-03 11:30:39 -0400

    Small changes

--- source/objects/StrumNote.hx
+++ source/objects/StrumNote.hx
@@ -22,11 +22,10 @@ class StrumNote extends FlxSprite
 		return value;
 	}
 
-	public var useRGBShader:Bool = true;
+	public var useRGBShader:Bool = false;
 	public function new(x:Float, y:Float, leData:Int, player:Int) {
 		rgbShader = new RGBShaderReference(this, Note.initializeGlobalRGBShader(leData));
 		rgbShader.enabled = false;
-		if(PlayState.SONG != null && PlayState.SONG.disableNoteRGB) useRGBShader = false;
 		
 		var arr:Array<FlxColor> = ClientPrefs.data.arrowRGB[leData];
 		if(PlayState.isPixelStage) arr = ClientPrefs.data.arrowRGBPixel[leData];
