11d4e383f989388385fd35a4d46b2734f3c81bea
Nikogamer184 <nicsteph547@gmail.com>
2023-08-03 21:31:40 -0400

    Update

--- source/custom/CustomScript.hx
+++ source/custom/CustomScript.hx
@@ -8,9 +8,12 @@ class CustomScript
 {
 	var game:PlayState;
 
+	var localSprites:Array<FlxSprite>;
+
 	public function new()
 	{
 		game = PlayState.instance;
+		localSprites = [];
 	}
 
 	function addSpriteToStage(sprite:FlxSprite) // addLuaSprite()
@@ -40,7 +43,6 @@ class CustomScript
 			{
 				case 'SET ME FREE':
 					game.camGame.flash(0xFF000000, 10, null, false);
-					game.skipCountdown = true;
 					game.cameraSpeed = 10;
 					game.camHUD.alpha = 0;
 					game.dadGroup.alpha = 0.0001;
@@ -48,6 +50,8 @@ class CustomScript
 
 					var loading:FlxSprite = new FlxSprite(1150, 880).loadGraphic(Paths.image('backgrounds/flanima/loading'));
 					var noway:FlxSprite = new FlxSprite(-600, -300).makeGraphic(2600, 1600, 0xFFCBFD56);
+					localSprites.insert(0, loading);
+
 					addSpriteToStage(noway);
 					game.add(loading);
 				default:
@@ -72,6 +76,27 @@ class CustomScript
 									game.modchartTweens.remove('appear');
 								}
 							}));
+						case 192:
+							game.modchartTweens.set('oh?', FlxTween.tween(game.camGame, {zoom: 1.25}, 5, {
+								ease: FlxEase.sineInOut,
+								onComplete: function(twn:FlxTween)
+								{
+									game.modchartTweens.remove('oh?');
+								}
+							}));
+							game.modchartTweens.set('loadingfade', FlxTween.tween(localSprites[0], {alpha: 0}, 4, {
+								onComplete: function(twn:FlxTween)
+								{
+									game.modchartTweens.remove('loadingfade');
+								}
+							}));
+						case 248:
+							game.cameraSpeed = 5;
+							game.defaultCamZoom = 1.1;
+							game.dadGroup.alpha = 1;
+
+							game.boyfriendCameraOffset[0] = -50;
+							game.boyfriendCameraOffset[1] = 0;
 					}
 				default:
 					return;
